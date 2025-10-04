e61a8007d4de470d7bb2c9526aa081c4b91ea54b
Isac <66953921+isac531@users.noreply.github.com>
2025-10-04 03:05:00 -0300

    Update libtslog.c

--- libtslog.c
+++ libtslog.c
@@ -1,5 +1,4 @@
 #include "libtslog.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
@@ -62,4 +61,5 @@ void tslog_info(logger_t *logger, const char *message) {
 
 void tslog_error(logger_t *logger, const char *message) {
     write_log(logger, "ERROR", message);
+
 }
